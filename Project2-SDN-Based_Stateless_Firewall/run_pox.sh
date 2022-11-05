3fb74bedec70af26b8a00b5bf77efcbf4349ba76
Afshin Paydar <afshin.paydar@binary.com>
2022-11-05 13:05:47 +0800

    add full path to pox

--- Project2-SDN-Based_Stateless_Firewall/run_pox.sh
+++ Project2-SDN-Based_Stateless_Firewall/run_pox.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-cd /home/ubuntu/pox/
-
-nohup ./pox.py openflow.of_01 \
+nohup /home/ubuntu/pox/pox.py openflow.of_01 \
 	--port=6655 pox.forwarding.l2_learning \
 	pox.forwarding.L3Firewall --l2config="l2firewall.config" \
 	--l3config="l3firewall.config" &
 
-nohup ./pox.py openflow.of_01 \
+nohup /home/ubuntu/pox/pox.py openflow.of_01 \
 	--port=6633 pox.forwarding.l2_learning \
 	pox.forwarding.L3Firewall --l2config="l2firewall.config" \
 	--l3config="l3firewall.config" &
